079585f87ab9684d7e88ce1f0c957c246b9fec3c
bohdantaradada <reyyes98@gmail.com>
2024-05-07 15:33:33 +0300

    in progress

--- src/modules/email-confirmation/email-confirmation.module.ts
+++ src/modules/email-confirmation/email-confirmation.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { EmailConfirmationController } from './email-confirmation.controller';
 import { EmailConfirmationService } from './email-confirmation.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { User } from 'modules/user';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Verification } from 'modules/user/verification.entity';
+import { ConfigModule, ConfigService } from 'modules/config';
 
 @Module({
+  imports:[MailerModule.forRootAsync({
+    imports:[ConfigModule],
+    inject:[ConfigService],
+    useFactory: (configService:ConfigService) => {
+      return {
+        transport: {
+          host: configService.get("SMTP_HOST"),
+          port: Number(configService.get("SMTP_HOST")),
+          auth: {
+            user: configService.get("SMTP_USER"),
+            pass: configService.get("SMTP_PASS"),
+          },  
+        }
+      }
+    }
+  }),
+  TypeOrmModule.forFeature([User, Verification]),
+  ConfigModule
+],
   controllers: [EmailConfirmationController],
   providers: [EmailConfirmationService]
 })
-export class EmailConfirmationModule {}
+export class EmailConfirmationModule {
+}
--- src/modules/email-confirmation/email-confirmation.service.ts
+++ src/modules/email-confirmation/email-confirmation.service.ts
@@ -4,20 +4,24 @@ import { AppError, HttpCode } from 'exceptions/app-error';
 import { User } from 'modules/user';
 import { Verification } from 'modules/user/verification.entity';
 import { Repository } from 'typeorm';
-import nodemailer from "nodemailer";
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from 'modules/config';
+import * as dayjs from 'dayjs';
 
 @Injectable()
 export class EmailConfirmationService {
     constructor(
+        private readonly mailService:MailerService,
+        private readonly configService:ConfigService,
         @InjectRepository(User) private readonly userRepository: Repository<User>,
         @InjectRepository(Verification) private readonly verificationRepository: Repository<Verification>
     ){}
 
   
 
-    public async sendConfirmationEmail(userId: string): Promise<{
-        previousVerificaitonDates: Date[]
-      }> {
+    public async sendConfirmationEmail(userId: string):Promise<{
+      previousVerificaitonDates: Date[]
+    }>  {
         const user = await this.userRepository.findOne({
           where: { id: userId },
           relations: ["verifications"]
@@ -34,7 +38,7 @@ export class EmailConfirmationService {
           Math.floor(Math.random() * (max - min)) + min;
         const generateCode = () =>
           new Array(6).fill('').reduce(prev => prev + getRandomInt(0, 9), '');
-    
+
         const previousVerifications = await this.verificationRepository.createQueryBuilder("verification")
           .where("verification.userId = :userId", { userId: user.id })
           .orderBy("verification.createdAt", "DESC")
@@ -48,32 +52,59 @@ export class EmailConfirmationService {
         user.verifications.push(verification);
         
         await this.verificationRepository.save(verification);
-    
-        const mailConfig = {
-            host: process.env.SMTP_HOST,
-            // port: Number(process.env.SMTP_PORT) || 587,
-            port: Number(process.env.SMTP_PORT),
-           
-            auth: {
-              user: process.env.SMTP_USER,
-              pass: process.env.SMTP_PASS,
-            },
-          };
+
+        this.mailService.sendMail({
+          from:this.configService.get("SMTP_USER"),
+          to:user.email,
+          subject: 'Please confirm your email',
+          text: `Your verification code is ${verification.code}`,
+        })
       
-          const transporter = nodemailer.createTransport(mailConfig);
-      
-          await transporter.sendMail({
-            from: process.env.SMTP_USER,
-            to: 'reyyes98@gmail.com',
-            subject: 'Please confirm your email',
-            text: `Your verification code is ${verification.code}`,
-          });
-      
-          await this.userRepository.save(user);
-          return {
+        await this.userRepository.save(user);
+        return {
             previousVerificaitonDates: previousVerifications.map(v => v.createdAt)
-          }
         }
-      
+      }
+
+      public confirmEmail = async (confirmEmailDTO: {email:string, verification_code:string}) => {
+        const user = await this.userRepository.findOneOrFail({where: {email: confirmEmailDTO.email}});
+        if (!user) {
+          throw new AppError({
+            httpCode: HttpCode.NOT_FOUND,
+            description: `User with email ${confirmEmailDTO.email} does not exist`,
+          });
+        }
     
+        const latestVerification = await this.verificationRepository.createQueryBuilder("verification")
+          .where("verification.userId = :userId", { userId: user.id })
+          .orderBy("verification.createdAt", "DESC")
+          .getOne();
+    
+        if (!latestVerification) {
+          throw new AppError({
+            httpCode: HttpCode.FORBIDDEN,
+            description: `Incorrect code`,
+          });
+        }
+        
+        if (latestVerification.code === confirmEmailDTO.verification_code) {
+          const date1 = dayjs(new Date())
+          const date2 = dayjs(latestVerification.createdAt)
+          if (date1.diff(date2, 'hour') >= 1 ) {
+            throw new AppError({
+              httpCode: HttpCode.FORBIDDEN,
+              description: `Code is expired`,
+            });
+          }
+          user.is_email_verified = true;
+          await this.userRepository.save(user);
+        } else {
+          throw new AppError({
+            httpCode: HttpCode.FORBIDDEN,
+            description: `Verification code doesn't match`,
+          });
+        }
+        
+        return user
+      }
 }
