dea1245a2c251e3a0c253fcb654265d6bfdaefbc
yekotovskyi <yekotovskyi@gmail.com>
2024-05-16 13:18:07 +0300

    test without env

--- src/modules/config/config.service.ts
+++ src/modules/config/config.service.ts
@@ -4,20 +4,8 @@ import * as fs from 'fs';
 export class ConfigService {
   private readonly envConfig: { [key: string]: string };
 
-  constructor(filePath: string) {
-    console.log('=====', filePath)
-    const dir = fs.readdirSync('.');
-    console.log('Current dir', dir)
-    const parent = fs.readdirSync('./../');
-    console.log('Parent', parent)
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
-  }
-
   get(key: string): string {
-    return this.envConfig[key];
-  }
-
-  isEnv(env: string) {
-    return this.envConfig.APP_ENV === env;
+    console.log('CONFIG', key, process.env[key])
+    return process.env[key]
   }
 }
